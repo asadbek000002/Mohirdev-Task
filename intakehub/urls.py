a846f5fa2bc011f23620b8c663a108a7df53a173
asadbek000002 <asadbektuygunov9@gmail.com>
2025-05-22 11:23:27 +0500

    swagger ni asosiy url fileda loyhaga moslashtrdim

--- intakehub/urls.py
+++ intakehub/urls.py
@@ -8,22 +8,24 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Leads API",
+        title="Intakehub API",
         default_version='v1',
-        description="Leads management system with email notifications",
+        description="Intakehub project API",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@yourdomain.com"),
+        contact=openapi.Contact(email="asadbektuygunov9@snippets.local"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
     permission_classes=[permissions.AllowAny],
 )
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0),
+          name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('attorney/', include('attorney.urls')),
     path('intake/', include('intake.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
